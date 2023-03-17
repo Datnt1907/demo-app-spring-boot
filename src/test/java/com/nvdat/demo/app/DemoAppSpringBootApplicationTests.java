33914bc8853d4a8d998d979b6c508ffdcffd7a7b
neonampv <quinv@neosvietnam.vn>
2023-03-18 04:33:25 +0700

    init

---
+++ src/test/java/com/nvdat/demo/app/DemoAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvdat.demo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
